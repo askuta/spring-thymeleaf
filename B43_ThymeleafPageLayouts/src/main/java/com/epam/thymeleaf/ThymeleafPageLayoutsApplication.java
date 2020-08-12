79ed2c88ff4e43522f39ffb8480ee0a1e6bf0403
Zoltan Gabor <Zoltan_Gabor@epam.com>
2020-08-12 17:15:46 +0200

    Add project

---
+++ B43_ThymeleafPageLayouts/src/main/java/com/epam/thymeleaf/ThymeleafPageLayoutsApplication.java
@@ -0,0 +1,13 @@
+package com.epam.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPageLayoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafPageLayoutsApplication.class, args);
+	}
+
+}
